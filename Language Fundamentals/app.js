c14ab95690c372eb4a6fb4b1a8ab9925491c0f75
shivuram <sivasubramanian.ramnath@gmail.com>
2020-04-22 20:50:01 +0530

    Fundamentals: Strings Methods

--- Language Fundamentals/app.js
+++ Language Fundamentals/app.js
@@ -44,6 +44,62 @@ val = Number('5');
 
 //String to Integer
 val = parseInt('2.70');
-console.log(val);
+//console.log(val);
 val = parseFloat('100.30677');
-console.log(val.toFixed(2));
+//console.log(val.toFixed(2));
+
+//---------------------- Math Object --------------------------------
+let math;
+math = Math.floor(2.89999999);
+math = Math.min(2, 5);
+math = Math.max(2,33,4,1,55,6,3,-2);
+math = Math.random();
+console.log(math);
+
+//-----------------------Stings--------------------------------------
+
+const firstName = 'William';
+const lastName = 'Johnson';
+
+// Concatenation
+str = firstName + ' ' + lastName;
+
+//Append
+str += 'Hello';
+
+// Escaping
+str =  'That\'s awesome, I can\'t wait';
+
+//Length
+str = firstName.length;
+
+//concat()
+str = firstName.concat(' ', lastName);
+
+//Change Case
+str = firstName.toLowerCase();
+str = firstName.toUpperCase();
+
+str = firstName.indexOf('i',2);
+str = firstName.lastIndexOf('a');
+
+// charAt()
+str = firstName.charAt('1');
+
+// substring()
+str = firstName.substring(1,4);
+
+// slice()
+str = firstName.slice(1,4);
+
+//split()
+str = firstName.split(','); //Give an Array like
+
+//replace()
+str = firstName.replace('Wi', 'We');
+
+// includes()
+str = firstName.includes('W'); //True
+str = firstName.includes('w'); //False
+
+console.log(str);
